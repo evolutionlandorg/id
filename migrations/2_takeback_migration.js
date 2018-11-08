736df3c638c40340ff91734dadd449f82df67010
董淑伟 <dsw0602@foxmail.com>
2018-11-08 16:52:26 +0800

    update migration

--- migrations/2_takeback_migration.js
+++ migrations/2_takeback_migration.js
@@ -1,4 +1,3 @@
-const ChannelDividend = artifacts.require('ChannelDividend');
 const DividendPool = artifacts.require('DividendPool');
 const FrozenDividend = artifacts.require('FrozenDividend');
 const IDSettingIds = artifacts.require('IDSettingIds');
@@ -6,23 +5,28 @@ const SettingsRegistry = artifacts.require('SettingsRegistry');
 const MintAndBurnAuthority = artifacts.require('MintAndBurnAuthority');
 const StandardERC223 = artifacts.require('StandardERC223');
 const Proxy = artifacts.require("OwnedUpgradeabilityProxy");
+const RolesUpdater = artifacts.require("RolesUpdater");
+const UserRoles = artifacts.require("UserRoles");
+const UserRolesAuthority = artifacts.require("UserRolesAuthority");
 
 var conf = {
     registry_address: '0xd8b7a3f6076872c2c37fb4d5cbfeb5bf45826ed7',
     auctionSettingIds_address: '0x5e5062115a5056b6d6f167538f5572d71cd0bf30',
     ring_address: '0xf8720eb6ad4a530cccb696043a0d10831e2ff60e',
-    supervisor_address: '0x4cC4c344ebA849DC09ac9Af4bfF1977e44FC1D7E',
+    supervisor_address: '0x00a1537d251a6a4c4effAb76948899061FeA47b9',
     bankProxy_address: '0x6436b1eb4b71616202620ccc2e974d6c02b5a3b1',
-    kton_address: '0xf8c63be35fea3679e825df8ce100dd2283f977c7'
+    kton_address: '0xf8c63be35fea3679e825df8ce100dd2283f977c7',
+    takeback_address: '0xa0feeb22a4f02e4e10e4dbd847f8cde521d97434',
+    networkId: 42
 }
 
 let dividendPoolProxy_address;
 let frozenDividendProxy_address;
+let userRolesProxy_address;
 
 
-
-module.exports = function(deployer, network, accounts) {
-    if (network != "kovan") {
+module.exports = function (deployer, network, accounts) {
+    if (network == "kovan") {
         // // TODO;
         // var tokenAddress = "0x86E56f3c89a14528858e58B3De48c074538BAf2c";
         // deployer.deploy(TakeBack,tokenAddress,accounts[0],2);
@@ -32,20 +36,29 @@ module.exports = function(deployer, network, accounts) {
 
 
     deployer.deploy(IDSettingIds);
-    deployer.deploy(ChannelDividend, conf.ring_address, conf.supervisor_address, 42);
-    deployer.deploy(Proxy).then(async() => {
+    deployer.deploy(Proxy).then(async () => {
         let dividendPoolProxy = await Proxy.deployed();
         dividendPoolProxy_address = dividendPoolProxy.address;
         console.log('DividendPoolProxy address: ', dividendPoolProxy_address);
         await deployer.deploy(DividendPool);
         await deployer.deploy(Proxy);
-    }).then(async() => {
+    }).then(async () => {
         let frozenDividendProxy = await Proxy.deployed();
         frozenDividendProxy_address = frozenDividendProxy.address;
         console.log('frozenDividendProxy address: ', frozenDividendProxy_address);
         await deployer.deploy(FrozenDividend);
         await deployer.deploy(MintAndBurnAuthority, [conf.bankProxy_address, dividendPoolProxy_address]);
-    }).then(async() => {
+        await  deployer.deploy(Proxy)
+    }).then(async () => {
+        let userRolesProxy = await Proxy.deployed();
+        userRolesProxy_address = userRolesProxy.address;
+        console.log('UserRolesProxy address:', userRolesProxy_address);
+        await deployer.deploy(UserRoles);
+    }).then(async () => {
+        await deployer.deploy(RolesUpdater, userRolesProxy_address, conf.networkId, conf.supervisor_address);
+    }).then(async () => {
+        await deployer.deploy(UserRolesAuthority, [RolesUpdater.address]);
+    }).then(async () => {
 
         let settingIds = await IDSettingIds.deployed();
         let registry = await SettingsRegistry.at(conf.registry_address);
@@ -55,7 +68,7 @@ module.exports = function(deployer, network, accounts) {
         await registry.setAddressProperty(dividendPoolId, dividendPoolProxy_address);
 
         let channelDivId = await settingIds.CONTRACT_CHANNEL_DIVIDEND.call();
-        await registry.setAddressProperty(channelDivId, ChannelDividend.address);
+        await registry.setAddressProperty(channelDivId, conf.takeback_address);
 
         let frozenDivId = await settingIds.CONTRACT_FROZEN_DIVIDEND.call();
         await registry.setAddressProperty(frozenDivId, frozenDividendProxy_address);
@@ -65,6 +78,7 @@ module.exports = function(deployer, network, accounts) {
         // upgrade
         await Proxy.at(dividendPoolProxy_address).upgradeTo(DividendPool.address);
         await Proxy.at(frozenDividendProxy_address).upgradeTo(FrozenDividend.address);
+        await Proxy.at(userRolesProxy_address).upgradeTo(UserRoles.address);
 
         console.log("UPGRADE DONE! ");
 
@@ -75,9 +89,16 @@ module.exports = function(deployer, network, accounts) {
         let frozenDividendProxy = await FrozenDividend.at(frozenDividendProxy_address);
         await frozenDividendProxy.initializeContract(conf.registry_address);
 
+        let userRolesProxy = await UserRoles.at(userRolesProxy_address);
+        await userRolesProxy.initializeContract();
+
+        console.log("INITIALIZATION DONE! ");
+
         // setAuthority
         let kton = await StandardERC223.at(conf.kton_address);
         await kton.setAuthority(MintAndBurnAuthority.address);
+
+        await userRolesProxy.setAuthority(UserRolesAuthority.address);
 
         console.log('MIGRATION SUCCESS!');
 
@@ -85,5 +106,4 @@ module.exports = function(deployer, network, accounts) {
     })
 
 
-    
 };
