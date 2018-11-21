809576d8138a2a2681bf5671c9c9f19cba9f3201
HackFisher <hackfisher@gmail.com>
2018-11-21 14:11:24 +0800

    RedBag related contract

---
+++ contracts/RedBag.sol
@@ -0,0 +1,90 @@
+pragma solidity ^0.4.24;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "@evolutionland/common/contracts/interfaces/ISettingsRegistry.sol";
+import "./IDSettingIds.sol";
+
+contract RedBag is Pausable, IDSettingIds {
+    using SafeMath for *;
+
+    event RegBagCreated(uint256 indexed id, address indexed creator, uint256 count, uint256 value, uint256 model);
+
+    event ClaimedTokens(address indexed _token, address indexed _to, uint _amount);
+
+    ISettingsRegistry public registry;
+
+    uint256 public ringAmountLimit;
+    uint256 public bagCountLimit;
+
+    uint256 public count;
+
+    constructor(ISettingsRegistry _registry, uint256 _ringAmountLimit, uint256 _bagCountLimit) public
+    {
+        require(_ringAmountLimit > 0, "RING amount need to be no-zero");
+        require(_bagCountLimit > 0, "RING bag amount need to be no-zero");
+
+        registry = _registry;
+        ringAmountLimit = _ringAmountLimit;
+        bagCountLimit = _bagCountLimit;
+    }
+
+    function tokenFallback(address _from, uint256 _value, bytes _data) public whenNotPaused {
+        if (msg.sender == registry.addressOf(CONTRACT_RING_ERC20_TOKEN)) {
+            uint256 _bagCount;
+            uint256 _model;
+            assembly {
+                let ptr := mload(0x40)
+                calldatacopy(ptr, 0, calldatasize)
+                _bagCount := mload(add(ptr, 132))
+                _model := mload(add(ptr, 164))
+            }
+
+            _giveRedBag(_from, _value, _bagCount, _model);
+        }
+    }
+
+    function _giveRedBag(address _sender, uint256 _ringAmount, uint256 _bagCount, uint256 _model) internal whenNotPaused {
+        require(_sender != address(0));
+        require(_ringAmount <= ringAmountLimit && _ringAmount > 0);
+        require(_bagCount <= bagCountLimit && _bagCount > 0);
+
+        ERC20(registry.addressOf(CONTRACT_RING_ERC20_TOKEN)).transfer(registry.addressOf(CONTRACT_CHANNEL_DIVIDEND), _ringAmount);
+
+        count += 1;
+
+        emit RegBagCreated(count, _sender, _bagCount, _ringAmount, _model);
+    }
+
+    function changeRegistry(ISettingsRegistry _newRegistry) public onlyOwner {
+        registry = _newRegistry;
+    }
+
+    function changeLimit(uint256 _ringAmountLimit, uint256 _bagCountLimit) public onlyOwner {
+        ringAmountLimit = _ringAmountLimit;
+        bagCountLimit = _bagCountLimit;
+    }
+
+
+//////////
+// Safety Methods
+//////////
+
+    /// @notice This method can be used by the controller to extract mistakenly
+    ///  sent tokens to this contract.
+    /// @param _token The address of the token contract that you want to recover
+    ///  set to 0 in case you want to extract ether.
+    function claimTokens(address _token) onlyOwner public {
+      if (_token == 0x0) {
+          owner.transfer(address(this).balance);
+          return;
+      }
+
+      ERC20 token = ERC20(_token);
+      uint balance = token.balanceOf(this);
+      token.transfer(owner, balance);
+
+      emit ClaimedTokens(_token, owner, balance);
+    }
+}
